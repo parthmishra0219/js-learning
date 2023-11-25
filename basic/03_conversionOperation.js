8b349c1176ecdd50ae3ab219756ec0c9ddda0b25
Parth Sarthy <parthmishra0219@gmail.com>
2023-11-25 06:10:45 +0000

    conversion of datatype

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = null
+
+
+console.log(typeof( score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1, false= 0
+// undefined = NaN
+// string = NaN
+// null =0
+
+
+let isLoggedIn = "4"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//"" =false
+// "parth" = true
+
+
+let someNumber = 33
+
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber )
