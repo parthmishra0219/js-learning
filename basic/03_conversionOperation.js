cd55ca6ffa566051755c32e076cfee25e856c3ed
Parth Sarthy <parthmishra0219@gmail.com>
2023-11-25 07:44:22 +0000

    comparision

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null
 
 
-console.log(typeof( score));
+// console.log(typeof( score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
 
 // "33" = 33
@@ -19,7 +19,7 @@ console.log( valueInNumber);
 let isLoggedIn = "4"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //"" =false
 // "parth" = true
@@ -28,5 +28,29 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber )
+// console.log(stringNumber);
+// console.log(typeof stringNumber )
+
+
+
+//**************************************************   Operations   *********************************************************************************** */
+
+
+let value = 4
+let negValue = -value;
+// console.log(negValue);  // negValue= -4
+
+
+let str1 = "hello"
+ str2= " world"
+
+ let str3 = str1 +str2;
+ console.log(str3);
+
+ console.log("1" + 2);           // 12
+ console.log(1 + "2");          // 12
+ console.log("1" + 2 + 2);     // 122
+ console.log(1 + 2 + "2");    //  32
+
+ 
+      
