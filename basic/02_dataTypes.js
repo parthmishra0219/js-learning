7ee128a7377f41ba89af08b6460a52a22e321692
Parth Sarthy <parthmishra0219@gmail.com>
2023-11-25 05:51:05 +0000

    Datatypes

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+//alert( 3+3) //we r using node.js not browser
+
+let name ="parth"
+let age = 23
+let isLoggedIn = false
+
+
+/*
+dataTypes
+
+primitive datatypes 
+
+number = 2 to power 53
+strng = " "
+boolean = true/false
+bigint
+null = standalone value
+undefined = value didn't assigned
+symbol = uniqueness
+
+
+object
+*/
+
+
+console.log(typeof null);  //object 
+console.log(typeof undefined);   //undefined
