dc5593f6648c3a8189ba19b01bf52b748a2fb446
Parth Sarthy <parthmishra0219@gmail.com>
2023-11-25 07:47:09 +0000

    comparision

--- basic/04_comparision.js
+++ basic/04_comparision.js
@@ -1,10 +1,15 @@
-console.log("2" > 1);          // true
-console.log("02" > 1);        //  ture
+// console.log("2" > 1);          // true
+// console.log("02" > 1);        //  ture
 
-console.log( null > 0);      // false
-console.log( null == 0);    //  false
-console.log( null >= 0);   //   true
+// console.log( null > 0);      // false
+// console.log( null == 0);    //  false
+// console.log( null >= 0);   //   true
 
-console.log(undefined == 0);         // false      
-console.log(undefined > 0);         //  false
-console.log(undefined < 0);        //   false
+// console.log(undefined == 0);         // false      
+// console.log(undefined > 0);         //  false
+// console.log(undefined < 0);        //   false
+
+
+//Strick check ( === )
+
+console.log("2" === 2);
