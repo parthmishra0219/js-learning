6856a9d7e815e41d0e2c8cb79ed34494eec95f8e
Parth Sarthy <parthmishra0219@gmail.com>
2023-11-28 12:54:19 +0000

    regular function and function expression

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,7 +7,7 @@ if (true) {
     const b = 20
     var   c = 30
 
-    console.log("Inner:", a);
+    // console.log("Inner:", a);
 }
 
 
@@ -15,11 +15,66 @@ if (true) {
 
 
 // console.log(a);        // a is not defined
- console.log(a);       
+//  console.log(a);       // a=300
 // console.log(b);          //  b is not defined
 // console.log(c);      //     output =30 ( thats where scope needed in javascript because "var" does not understand block scope)
 
 
+function one() {
+    const username = "Parth"
+
+    function two() {
+        const website = " youTube"
+        console.log(username);
+
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
 
 
+if (true) {
+    const username = "parth sarthy"
+    if (username === "parth sarthy") {
+
+        const website = " youTube"
+        // console.log(username+ website);
+
+        
+    }
+    // console.log(website);    // website is not define in the scope 
+    
+}
+// console.log(username);     //   username is not define in the scope
+
+
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++     interesting     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Regular function 
+
+console.log(addone(5));   //output = 6 (hoisting happen )
+
+function addone(num) {
+    return num+1
+    
+}
+
+
+
+
+console.log(addTwo(5));     //output= Cannot access 'addTwo' before initialization ( hoisting does not happen in function expression)
+
+// function expression
+const addTwo = function (num) {
+    return num+2
+}
 
