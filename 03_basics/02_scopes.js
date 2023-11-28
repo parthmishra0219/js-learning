ed3ea7091cd781d2ce47a3a59a0c4891fbadca2e
Parth Sarthy <parthmishra0219@gmail.com>
2023-11-28 12:00:04 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,25 @@
+// // var c = 300
+let a = 300
+
+
+if (true) {
+    let   a = 10
+    const b = 20
+    var   c = 30
+
+    console.log("Inner:", a);
+}
+
+
+
+
+
+// console.log(a);        // a is not defined
+ console.log(a);       
+// console.log(b);          //  b is not defined
+// console.log(c);      //     output =30 ( thats where scope needed in javascript because "var" does not understand block scope)
+
+
+
+
+
