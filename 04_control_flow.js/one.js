4a1cc2e8c05babc143932de937e1139b0cfca696
Parth Sarthy <parthmishra0219@gmail.com>
2023-11-30 08:41:56 +0000

    basic if ,if else ,else if statement

--- 04_control_flow.js/one.js
+++ 04_control_flow.js/one.js
@@ -9,22 +9,88 @@
 
 
 // const isUserLoggedIn = true
-const temp = 30
+// const temp = 55
 
-if (temp < 40) {
+// if (temp === 41) {
 
-    console.log("temp less than 40");
-} 
-else {
-    console.log("temp greater than 40");
-}
+//     console.log("temp less than 50");
+// } 
+// else {
+//     console.log("temp greater than 50");
+// }
    
 
 
-
-
-
-
 // < , > , <= , >= , == , != , ===
+
+//block scope
+
+// const score =200
+
+// if(score > 100) {
+//     const power ="fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+
+
+
+
+// const balance=1000
+
+// if(balance >500) console.log("test");
+
+
+// if(balance < 500){
+//     console.log("less than 1000");
+
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }
+// else if(balance < 900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("grater than 950");
+// }
+
+
+
+const userLoggedIn = true
+const debitCard =true
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+
+// if(userLoggedIn && debitCard){
+//     console.log("allow to buy course");
+// }
+
+
+// if(loggedInFromEmail || loggedInFromGoogle){
+//     console.log("user looged in");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
