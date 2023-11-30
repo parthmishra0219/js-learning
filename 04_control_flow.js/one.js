3c0ec0ec290fac165d69a4bc5f445530a9ee296e
Parth Sarthy <parthmishra0219@gmail.com>
2023-11-30 08:13:05 +0000

    basic

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,30 @@
+// if statement
+
+// if (true) {
+
+// }
+// if (false) {
+
+// }
+
+
+// const isUserLoggedIn = true
+const temp = 30
+
+if (temp < 40) {
+
+    console.log("temp less than 40");
+} 
+else {
+    console.log("temp greater than 40");
+}
+   
+
+
+
+
+
+
+// < , > , <= , >= , == , != , ===
+
+
