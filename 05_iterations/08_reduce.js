ffcb39b361c54a4c36785449af8bc148b37e1a6a
Parth Sarthy <parthmishra0219@gmail.com>
2023-12-02 07:07:42 +0000

    using Reduce method in Arrays

---
+++ 05_iterations/08_reduce.js
@@ -0,0 +1,46 @@
+const myNums =[1,2,3,4,5]
+
+//  const myTotal = myNums.reduce(function (acc ,currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+//  } ,0 )
+
+
+// const myTotal =myNums.reduce( (acc,currval) => (acc+currval),0)
+
+
+//  console.log(myTotal);
+
+
+
+const shoppingCart =[
+    {
+        itemName:"javascript course",
+        price: 2999
+    },
+    {
+        itemName:"python course",
+        price: 999
+    },
+    {
+        itemName:"mobile dev course",
+        price: 5999
+    },
+    {
+        itemName:"data science course",
+        price: 12999
+    },
+    // {
+    //     itemName:"java course",
+    //     price: 6999
+    // }
+]
+
+
+
+//   const  priceToPay = shoppingCart.reduce( (acc ,item) =>(acc+(item.price)),0)
+//   console.log(priceToPay);
+
+
+  const  priceToPay = shoppingCart.reduce( (acc ,item) =>acc+(item.price),0)
+  console.log(priceToPay);
