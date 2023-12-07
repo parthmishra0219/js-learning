d3be1233d4e7be4a4a7c233afd87dccb33dc4ffb
Parth Sarthy <parthmishra0219@gmail.com>
2023-12-07 07:41:16 +0000

    guess Random number

---
+++ 07_projects/04_guessRandomNo/guessRandomNo.js
@@ -0,0 +1,120 @@
+
+let randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+// if (playGame) {
+//   submit.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const guess = parseInt(userInput.value);
+//     console.log(guess);
+//     validateGuess(guess);
+//   });
+// }
+
+if (playGame) {
+  submit.addEventListener('click', function(e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
+  });
+}
+
+
+
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert('plz enter a valid no.');
+  } else if (guess < 1) {
+    alert('plz enter the no. greater then 1');
+  } else if (guess > 100) {
+    alert('plz enter the no. less then 100');
+  } else {
+    prevGuess.push(guess);
+    if (numGuess === 10) {
+      displayGuess(guess);
+      displayMassege(`Game over. Ramdom no. was ${randomNumber}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
+
+
+function checkGuess(guess) {
+  if (guess === randomNumber) {
+    displayMassege(`you guess it right`);
+    endGame();
+  } else if (guess < randomNumber) {
+    displayMassege(`you guess is too low`);
+  } else if (guess > randomNumber) {
+    displayMassege(`you guess is too high`);
+  }
+}
+
+
+
+
+function displayGuess(guess) {
+  userInput.value = '';
+  guessSlot.innerHTML += `${guess} ,`;
+  numGuess++;
+  remaining.innerHTML = `${10 - numGuess}`
+}
+
+
+
+
+function displayMassege(message) {
+  lowOrHi.innerHTML = `<h2>${message}</h2>`;
+ }
+ 
+
+function endGame() {
+  userInput.value = '';
+  userInput.setAttribute('disabled', '');
+  p.classList.add('button');
+  p.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
+}
+
+function newGame() {
+  const newGameButton = document.querySelector('#newGame');
+  newGameButton.addEventListener('click', function (e) {
+    randomNumber = parseInt(Math.random() * 100 + 1);
+    prevGuess = [];
+    numGuess = 1;
+    guessSlot.innerHTML = '';
+    remaining.innerHTML = `${10 - numGuess} `;
+    userInput.removeAttribute('disabled');
+    startOver.removeChild(p);
+
+    playGame = true;
+  });
+}
+
+
+
+
+
+
+
+
+
