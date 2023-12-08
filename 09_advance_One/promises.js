35ed5cb28bf07ed6338deb6b43551f43fcba5792
Parth Sarthy <parthmishra0219@gmail.com>
2023-12-08 17:21:15 +0000

    promises

--- 09_advance_One/promises.js
+++ 09_advance_One/promises.js
@@ -82,6 +82,46 @@ const promiseFive = new Promise(function(resolve,reject){
 
 });
 
+async function consumePromiseFive(){
+   try {
+    const response = await promiseFive
+   console.log(response);
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+
+consumePromiseFive()
+
+
+
+// async function getAllUser(){
+//    try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//     // console.log(response);
+//   const data = await response.json()
+//   console.log(data);
+//    } catch (error) {
+//     console.log('Error :',error);
+//    }
+// }
+
+// getAllUser()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log("Error", error);
+})
+
+
+
+
 
 
 
