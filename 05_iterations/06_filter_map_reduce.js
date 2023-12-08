6725b597860c74d948a924424d7be541cf132f56
Parth Sarthy <parthmishra0219@gmail.com>
2023-12-08 09:53:09 +0000

    setTimeut,setIntervel

--- 05_iterations/06_filter_map_reduce.js
+++ 05_iterations/06_filter_map_reduce.js
@@ -9,11 +9,13 @@
 
 
 
-const myNums = [1,2,3,4,5,6,7,8,9,10]
+const myNums = [1,2,3,4,5,6,7,8,9,10,8,5,7]
 
-//   const newNums = myNums.filter( (num) => num>4)
+  const newNums = myNums.filter((num) => num>4)
 
-//   console.log(newNums);
+  
+
+  console.log(newNums);
 
 
 
