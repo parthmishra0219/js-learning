46552df8f7a2f048ad85e71c80c237d2ec7b2d99
Parth Sarthy <parthmishra0219@gmail.com>
2025-11-17 23:50:47 +0530

    Simplify background color change handling
    Refactor color change logic to eliminate redundancy.

--- 07_projects/01_colorChanger/colorChanger.js
+++ 07_projects/01_colorChanger/colorChanger.js
@@ -10,25 +10,25 @@ buttons.forEach(function (button) {
     console.log(e.target);
 
     if(e.target.id ==="grey"){
-    body.style.backgroundColor = e.target.id 
+    body.style.backgroundColor = e.target.id ;
     }
     if(e.target.id ==="green"){
-    body.style.backgroundColor = e.target.id 
+    body.style.backgroundColor = e.target.id ;
     }
     if(e.target.id ==="yellow"){
-    body.style.backgroundColor = e.target.id 
+    body.style.backgroundColor = e.target.id ;
     }
     if(e.target.id ==="blue"){
-    body.style.backgroundColor = e.target.id 
+    body.style.backgroundColor = e.target.id ;
     }
     if(e.target.id ==="red"){
-    body.style.backgroundColor = e.target.id 
+    body.style.backgroundColor = e.target.id ;
     }
     if(e.target.id ==="purple"){
-    body.style.backgroundColor = e.target.id 
+    body.style.backgroundColor = e.target.id ;
     }
     else{
-    body.style.backgroundColor = e.target.id 
+    body.style.backgroundColor = e.target.id ;
     }
 
 
